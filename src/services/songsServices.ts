dea4bac84150144da033b721863ccf1550fd0ae3
Pedro Henrique <phcatanduba@gmail.com>
2021-07-23 17:14:27 -0300

    fixs

--- src/services/songsServices.ts
+++ src/services/songsServices.ts
@@ -31,8 +31,8 @@ export async function hasId(id: number) {
 export async function hasThisSong(name: string, link: string) {
     const resultName = await songsRepositories.name(name);
     const resultLink = await songsRepositories.link(link);
+    const has = resultName.length !== 0 || resultLink.length !== 0;
 
-    const has = resultName.length !== 0 && resultLink.length !== 0;
     return has;
 }
 
