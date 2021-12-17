90c1a2c25790a91478e2985984b2b71cb0da5ae1
Pedro Henrique <phcatanduba@gmail.com>
2021-12-17 13:39:19 -0300

    prepare for deploy

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,23 @@ import cors from 'cors';
 import "reflect-metadata"
 import connection from './database';
 import * as songsControllers from './controllers/songsControllers';
+import {Request, Response} from 'express'
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/test', (req, res) => {
+app.get('/test', (req: Request, res: Response) => {
     res.send('OK!');
 });
 
 app.post('/recommendations', songsControllers.recommend);
 
-app.post('/recommendations/:id/upvote', (req, res) => {
+app.post('/recommendations/:id/upvote', (req: Request, res: Response) => {
     songsControllers.vote(req, res, 'upvote');
 });
 
-app.post('/recommendations/:id/downvote', (req, res) => {
+app.post('/recommendations/:id/downvote', (req: Request, res: Response) => {
     songsControllers.vote(req, res, 'downvote');
 });
 
