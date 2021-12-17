54d3143b9200b9c314c12a377fafbf995104b68a
Pedro Henrique <phcatanduba@gmail.com>
2021-12-17 12:54:50 -0300

    prepare for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import "./setup";
 import app, { init } from "./app";
 
+const port = +process.env.PORT || 4000;
+
 init().then(() => {
-  app.listen(+process.env.PORT || 4000, () => {
+  app.listen(port, () => {
     console.log('Server is listening on port 4000');
   });
 });
